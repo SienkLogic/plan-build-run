a1830c9230ba7ab1ea693a7ff2d36e786d5878d3
David Sienkowski <dave@sienkowski.com>
2026-02-24 09:21:46 -0500

    feat(35-01): GREEN - add escapeHtml helper and use it in HTMX error handler path

--- dashboard/src/middleware/errorHandler.js
+++ dashboard/src/middleware/errorHandler.js
@@ -3,6 +3,16 @@
  * MUST have exactly 4 parameters for Express to recognize it as an error handler.
  */
 
+function escapeHtml(str) {
+  if (!str) return '';
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+}
+
 // eslint-disable-next-line no-unused-vars
 export default function errorHandler(err, req, res, next) {
   // If headers already sent, delegate to Express default handler
@@ -40,9 +50,9 @@ export default function errorHandler(err, req, res, next) {
 
   // Render response
   if (isHtmx) {
-    let html = `<h1>Error ${status}</h1><p>${templateData.message}</p>`;
+    let html = `<h1>Error ${status}</h1><p>${escapeHtml(templateData.message)}</p>`;
     if (templateData.stack) {
-      html += `<pre><code>${templateData.stack}</code></pre>`;
+      html += `<pre><code>${escapeHtml(templateData.stack)}</code></pre>`;
     }
     html += '<p><a href="/">Return to Dashboard</a></p>';
     return res.status(status).send(html);
